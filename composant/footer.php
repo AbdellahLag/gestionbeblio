21f1bafcb74eca8d7f43d82537207a45fc98435a
AbdellahLag <98743951+AbdellahLag@users.noreply.github.com>
2023-04-13 10:10:49 +0000

    Add files via upload

---
+++ composant/footer.php
@@ -0,0 +1,45 @@
+<div class="foo">
+    <div class="col-lg-4">
+
+      <div class="social">
+        <ul>
+          <li><a href="#" ><img src="IMG/fb.png"><span>Facebook</span></a></li>
+          <li><a href="#" ><img src="IMG/go.png"><span>Google+</span></a></li>
+          <li><a href="#" ><img src="IMG/tw.png"><span>Twitter</span></a></li>
+          <li><a href="#" ><img src="IMG/yt.png"><span>Youtube</span></a></li>
+        </ul>
+      </div>
+
+    </div>
+
+    <div class="col-lg-4">
+      <div class="propos">
+        <h2 class="foo-headline">A propos</h2>
+        <p>Ce site a ete concu dans le cadre des mini projects <br><a href="admin/index.php">Administration</a>
+      </p>
+        
+      </div>
+    </div>
+    
+    <div class="col-lg-4">
+
+      <div class="team">        
+        <ul>
+          <h2 class="foo-headline">Ce site est creer par:</h2>
+
+          <li><a herf="#">Abdellah Laghzaoui</a></li>
+          <li><a herf="#">Aymane el malki cherkaoui</a></li>
+        </ul>
+      </div>  
+
+    </div>
+    
+
+</div>
+<div class="rights" >
+  © 2023 copyrights <span class="italic"></span>
+</div>
+
+
+<script type="text/javascript" src="CSS/bootstrap/js/bootstrap.min.js">
+</script>
